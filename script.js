43b7a30c0437e4fbd69da7fb78cb20d9db26f596
Israel Abazie <you@example.com>
2022-08-17 12:05:36 -0700

    text-speech integration

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function listen() {
-    let inputArea = document.getElementById('input-area')
-    let outputArea = document.getElementById('output-area')
-  
-    var recognition = new webkitSpeechRecognition();
-    recognition.lang = "en-GB";
-    recognition.start();
-  
-    recognition.onresult = function(event) {
-      let transcript = event.results[0][0].transcript;
-      if (transcript.includes("hello")) {
-        outputArea.innerHTML = "Hello, User!"
-   } else if (transcript.includes("weather")) {
-       window.open("https://www.google.com/search?q=weather") 
-   } else {
-       outputArea.innerHTML = "I don't know what you mean."
-   }
-      inputArea.innerHTML = event.results[0][0].transcript;
+  let inputArea = document.getElementById("input-area");
+  let outputArea = document.getElementById("output-area");
+
+  var recognition = new webkitSpeechRecognition();
+  recognition.lang = "en-GB";
+  recognition.start();
+
+  recognition.onresult = function (event) {
+    let transcript = event.results[0][0].transcript;
+    if (transcript.includes("hello")) {
+      outputArea.innerHTML = "Hello, User!";
+      speechSynthesis.speak(new SpeechSynthesisUtterance(transcript));
+    } else if (transcript.includes("weather")) {
+      window.open("https://www.google.com/search?q=weather");
+    } else {
+      outputArea.innerHTML = "I don't know what you mean.";
     }
-  }
+    inputArea.innerHTML = event.results[0][0].transcript;
+  };
+}
