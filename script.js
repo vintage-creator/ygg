be8ba2cf4fa2761163562fe3909086af10f58bc2
vintage-creator <101069893+vintage-creator@users.noreply.github.com>
2022-08-18 23:22:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function listen() {
   recognition.start();
 
   recognition.onresult = function (event) {
-    const response = "NFT otherwise known as Non-Fungible Token is a digital file that is recorded on the blockchain. NFT occurs in various categories, namely: digital art, game assets, music, among others. These can be transferred (i.e. ownership) albeit very unique in its own right in contrast to cryptos which are fungible or interchangeable.";
+    const response = "Non-Fungible Token commonly called - NFT - is a digital file that is recorded on the blockchain. NFT occurs in various categories, namely: digital art, game assets, music, among others. These can be transferred (i.e. ownership) albeit very unique in its own right in contrast to cryptos which are fungible or interchangeable.";
     let transcript = event.results[0][0].transcript;
-    if (transcript.includes("What is N-F-T?")) {
+    if (transcript.includes("Non-Fungible Token")) {
       speechSynthesis.speak(new SpeechSynthesisUtterance(response));
-    } else if (transcript.includes("Do you have a full documentation?")) {
+    } else if (transcript.includes("full documentation")) {
       window.open("https://github.com/vintage-creator/ygg");
 
-    } else if (transcript.includes("Web-3")) {
+    } else if (transcript.includes("web 3")) {
       speechSynthesis.speak(new SpeechSynthesisUtterance("This is the internet our current web is transitioning to - noted for its decentralized and permissonless state - where users can own their contents."));
       
     } else {
